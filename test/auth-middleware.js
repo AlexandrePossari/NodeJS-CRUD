b46db63e5f2261f2a40127383fcd438b53f60908
Alexandre Possari <ac.possari@gmail.com>
2024-05-01 17:59:11 -0300

    test: auth middleware yield a userId after decoding token

--- test/auth-middleware.js
+++ test/auth-middleware.js
@@ -1,4 +1,6 @@
 const expect = require('chai').expect;
+const jwt = require('jsonwebtoken')
+const sinon = require('sinon')
 
 const authMiddleware = require('../middleware/is-auth');
 
@@ -24,11 +26,25 @@ describe('Auth middleware', function(){
     it('should throw an error if the token cannot be verified', function(){
         const req = {
             get: function(headerName){
-                return 'Bearer xyz'
+                return 'Bearer xyz';
             }
         };
         expect(authMiddleware.bind(this, req, {}, () => {})).to.throw();
     })
 
+    it('should yield a userId after decoding the token', function(){
+        const req = {
+            get: function(headerName){
+                return 'Bearer testestestest';
+            }
+        };
+        sinon.stub(jwt, 'verify');
+        jwt.verify.returns({userId: 'abc'});
+        authMiddleware(req, {}, () => {});
+        expect(req).to.have.property('userId');
+        expect(req).to.have.property('userId', 'abc');
+        expect(jwt.verify.called).to.be.true;
+        jwt.verify.restore();
+    })
 });
 
