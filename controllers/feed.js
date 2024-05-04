d59c056fc13f58f669cf7829024cd9b4a0f11cae
Alexandre Possari <ac.possari@gmail.com>
2024-05-03 22:23:38 -0300

    fix: update endpoint not letting creator update book

--- controllers/feed.js
+++ controllers/feed.js
@@ -89,7 +89,7 @@ exports.updateBook = (req, res, next) => {
             error.statusCode = 404;
             throw error;
         }
-        if(book.creator.toString() !== req.UserId){
+        if(book.creator.toString() !== req.userId){
             const error = new Error('Not authorized');
             error.statusCode = 403;
             throw error;
